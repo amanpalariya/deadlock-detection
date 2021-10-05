9d4bc30486e97c4cd70ab15fac41d2bb7ddf82a9
Aman Palariya <2019csb1068@iitrpr.ac.in>
2021-10-05 13:58:32 +0530

    Setup boilerplate code

---
+++ tests/tester.c
@@ -0,0 +1,25 @@
+#include <stdbool.h>
+#include <stdio.h>
+
+int total_tests = 0;
+int passed_tests = 0;
+int failed_tests = 0;
+
+void test_log(char* test_name, bool success) {
+    total_tests++;
+    if (success) {
+        printf("\033[0;32m[✓]: %s\033[0m\n", test_name);
+        passed_tests++;
+    } else {
+        printf("\033[0;31m[ ]: %s\033[0m\n", test_name);
+        failed_tests++;
+    }
+}
+
+void print_test_section(char* section) {
+    printf("\n\033[0;1m%s\033[0m\n\n", section);
+}
+
+int main(int argc, char** argv) {
+    return 0;
+}
